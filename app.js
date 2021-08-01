1fcbe476854384d30eed3a93426a0fd23666fb43
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-08-02 02:57:28 +0600

    link app.js and fix api prob
    link app.js and fix api prob

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //6b6000a40725f177f50be50f5c8087b8
 const url =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6b6000a40725f177f50be50f5c8087b8%page=1";
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6b6000a40725f177f50be50f5c8087b8&page=1";
 
 async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
+  console.log(data.results);
 }
+getMovies(url);
